efd59a106de0471d1d6a07b5d19e1eadec4f1946
Anthony COLAS <acolas@e2r8p3.42.fr>
2017-04-10 16:27:11 +0200

    ex06v2

--- ex06/ft_print_alphabet.c
+++ ex06/ft_print_alphabet.c
@@ -6,16 +6,11 @@
 /*   By: acolas <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/10 15:44:36 by acolas            #+#    #+#             */
-/*   Updated: 2017/04/10 16:07:46 by acolas           ###   ########.fr       */
+/*   Updated: 2017/04/10 16:26:50 by acolas           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
-
-void	ft_putchar(char c)
-{
-	write(1, &c, 1);
-}
+void	ft_putchar(char c);
 
 void	ft_print_alphabet(void)
 {
