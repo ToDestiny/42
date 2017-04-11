a78887ded4fd57403f11e07d9cebcc64a1ba0919
Anthony COLAS <acolas@e1r2p2.42.fr>
2017-04-11 18:17:07 +0200

    ..

---
+++ piscine-reloaded/ft_strdup.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: acolas <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/04/11 17:06:11 by acolas            #+#    #+#             */
+/*   Updated: 2017/04/11 17:10:54 by acolas           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+int		ft_strlen(char *str)
+{
+	int		len;
+
+	len = 0;
+	while (str[len])
+		len++;
+	return (len);
+}
+
+char	*ft_strdup(char *src)
+{
+	int		i;
+	char	*dest;
+	int		len;
+
+	i = 0;
+	len = ft_strlen(src);
+	dest = (char *)malloc(sizeof(*dest) * len + 1);
+	if (dest == 0)
+		return (0);
+	while (src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
